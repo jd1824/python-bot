a13f42c40df1297643e40f4f1a9eadcb9c0341d9
Jesus <jdserrano106@gmail.com>
2023-05-12 16:21:30 -0400

    bot2.0

--- index.py
+++ index.py
@@ -1,17 +1,47 @@
-import os
+import requests
+import json
 import discord
 from discord.ext import commands
 
+url = "https://pokeapi.co/api/v2/pokemon/"
 
-bot = commands.Bot(command_prefix='>', intents=discord.Intents.default())
+intents = discord.Intents.default()
+bot = commands.Bot(command_prefix='!', intents=intents)
+intents.message_content = True
+
+bot.event
+async def on_ready():
+    print(f'{bot.user.name} has connected to Discord!')
+
 
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
 
 @bot.command()
+async def poke(ctx, num: int):
+    url2 = url + str(num)
+    poke = requests.get(url2)
+    poke_s = poke.text
+    poke_r = json.loads(poke_s)
+   # print(poke_r["name"])
+  
+    await ctx.send(poke_r["name"])
+
+@bot.command()
 async def sum(ctx, num1: int, num2: int):
     await ctx.send(num1 + num2)
 
+@bot.command(name="info")
+async def info(ctx):
+    embed = discord.Embed(title="informacion", description="este es un pequeño bot de prueba en python", color=discord.Color.blue())
+    embed.add_field(name='campo1', value='pruebs', inline=False)
+    #embed.add_field(name='Campo 2', value='Valor 2', inline=False)
+    embed.set_footer(text='ejemplo')
 
-bot.run(os.environ.get("token"))
+    await ctx.send(embed=embed)
+
+
+
+
+bot.run("MTAxNjUxMDgxMTEwMDk1NDY1NQ.GGcO6F.Z9csvh6ivqSL6ybiIR9thSR_L5W6ah7wgpqwuM")
